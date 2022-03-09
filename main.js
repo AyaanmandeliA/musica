b046076ca511c6ba8ec76848616f6d2f5278c738
AyaanmandeliA <78735976+AyaanmandeliA@users.noreply.github.com>
2022-03-09 16:24:44 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 song1hp = "";
 song1ju = "";
 
+lwy = 0;
+lwx = 0;
+
+rwy = 0;
+rwx = 0;
+
 function setup(){
     canvas = createCanvas(450 ,390);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", getPoses);
 }
 
 function draw(){
@@ -18,6 +26,23 @@ function preload(){
 }
 function sonply(){
     song1hp.play();
+}
+function getPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        lwx = results[0].pose.leftWrist.x;
+        lwy = results[0].pose.leftWrist.y;
+        console.log("left wrist x = " + lwx + "left wrist y = " + lwy)
+
+        rwx = results[0].pose.rightWrist.x;
+        rwy = results[0].pose.rightWrist.y;
+        console.log("right wrist x = " + rwx + "right wrist y = " + rwy)
+
+    }
+
+}
+function modelLoaded() {
+    console.log("modal loaded");
 }
 function sonsly(){
     song1hp.pause();
